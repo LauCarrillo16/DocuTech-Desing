2b0722b411faedcf9dec570033c66103531be6f8
LauCarrillo16 <8.10carrillolaura@gmail.com>
2023-05-24 16:43:45 -0500

    2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("./conexion/conexionDB");
 const { createServer } = require('http');
 const cors = require("cors");
